1a5f4e506e46dcc1a67530c379b1c672c1629510
purniawan4905 <purniawan4905@gmail.com>
2024-12-31 19:01:52 +0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let chars, particles, canvas, ctx, w, h, current;
 let duration = 5000;
-let str = ['HAPPY', 'NEW', 'YEAR' , '2025'];
+let str = ['HAPPY', 'NEW', 'YEAR' , '2024'];
 
 init();
 resize();
